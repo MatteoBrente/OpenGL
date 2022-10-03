2e23396881816f6a7e638ec9d4253bbae49a8c7c
Matteo Brentegani <brenteganimatteo@gmail.com>
2022-10-03 15:07:46 +0200

    made pyramid rotate

--- src/main.cpp
+++ src/main.cpp
@@ -101,12 +101,13 @@ int main()
 
 
 	// Texture
-	Texture popCat("resources/textures/brick.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	Texture popCat("./resources/textures/kek.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGB, GL_UNSIGNED_BYTE);
 	popCat.texUnit(shaderProgram, "tex0", 0);
 
-	// Original code from the tutorial
-	/*Texture popCat("pop_cat.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
-	popCat.texUnit(shaderProgram, "tex0", 0);*/
+	float rotation = 0.0f;
+	double prevTime = glfwGetTime();
+
+	glEnable(GL_DEPTH_TEST);
 
 
 	// Main while loop
@@ -115,13 +116,21 @@ int main()
 		// Specify the color of the background
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		// Clean the back buffer and assign the new color to it
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		// Tell OpenGL which Shader Program we want to use
 		shaderProgram.Activate();
+
+		double crntTime = glfwGetTime();
+		if (crntTime - prevTime >= (1 / 60))
+		{
+			rotation += 0.5f;
+			prevTime = crntTime;
+		}
 
 		glm::mat4 model = glm::mat4(1.0f);
 		glm::mat4 view = glm::mat4(1.0f);
 		glm::mat4 proj = glm::mat4(1.0f);
+		model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
 		view = glm::translate (view, glm::vec3(0.0f, -0.5f, -2.0f));
 		proj = glm::perspective (glm::radians(45.0f), (float)(WIDTH / HEIGHT), 0.1f, 100.0f);
 
